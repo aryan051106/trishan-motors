0f095a52a36fce428cad8485cdc5c496e1d0fbbd
aryan051106 <patelaryan0511@gmail.com>
2026-03-28 22:09:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+// Trishan Motors EV Scooters Data
+// You can add more scooters later easily.
+
+const SCOOTERS = [
+  {
+    id: "single",
+    name: "Single Light",
+    price: 40000
+  },
+  {
+    id: "double",
+    name: "Double Light",
+    price: 63000
+  },
+  {
+    id: "nduro",
+    name: "Nduro",
+    price: 107000
+  }
+];
